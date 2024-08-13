42c09d0d8c04b75696e3bb14c1784bd79000f943
Aibek002 <200103346@stu.sdu.edu.kz>
2024-08-13 10:07:57 +0500

    Resolve merge conflicts

--- config/db.php
+++ config/db.php
@@ -2,9 +2,22 @@
 
 return [
     'class' => 'yii\db\Connection',
+<<<<<<< HEAD
     'dsn' => 'mysql:host=db;dbname=yii2basic', // mysql - имя сервиса MySQL в Docker
     'username' => 'yii2basic',
     'password' => 'yii2basic',
     'charset' => 'utf8',
 ];
 
+=======
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
+>>>>>>> 01b1df5 (	new file:   .bowerrc)
